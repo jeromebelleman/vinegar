7ef392bfcf47c37004433a7093f950275a3f5804
Jerome Belleman <jerome.belleman@gmail.com>
2019-03-28 10:25:06 +0100

    Parse steps

--- vinegar.js
+++ vinegar.js
@@ -1,5 +1,24 @@
 const fs = require('fs')
 
+const steps = ['Given', 'When', 'Then', 'And']
+const stepRegExps = steps.map(step => RegExp(`^ *(${step}) *(.*) *`))
+
+// Tests
 exports.load = function (feature, definition) {
-  return fs.readFileSync(feature).toString().split('\n')
+  const lines = fs.readFileSync(feature).toString().split('\n')
+
+  backgrounds = []
+  scenarios = []
+
+  lines.map(line => {
+    for (const stepRegExp of stepRegExps) {
+      const match = line.match(stepRegExp)
+      if (match) {
+        scenarios.push({ keyword: match[1], text: match[2] })
+        break
+      }
+    }
+  })
+
+  return { backgrounds, scenarios }
 }
